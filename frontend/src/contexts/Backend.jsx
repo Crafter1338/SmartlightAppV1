d251b67240e613a26c4f35428f2f8b0806886a6e
Crafter1338 <111569232+Crafter1338@users.noreply.github.com>
2025-06-19 15:05:28 +0200

    Update Backend.jsx

--- frontend/src/contexts/Backend.jsx
+++ frontend/src/contexts/Backend.jsx
@@ -3,8 +3,7 @@ import { io } from "socket.io-client";
 
 const Context = createContext(null);
 
-const ADRESS = "http://192.168.2.116:2000";
-//const ADRESS = "http://31.97.47.250:2000";
+const ADRESS = import.meta.env.VITE_BACKEND;
 
 export function BackendProvider ({ children }) {
 	const [socket, setSocket] = useState(null);
